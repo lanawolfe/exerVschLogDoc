fa8ffc679139137a6ca9a4bd3bd6d7964a9992b6
lanawolfe <lana.j.wolfe@utah.edu>
2020-11-18 11:08:28 -0700

    html webpage

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-const entryForm = document.getElementById('entryForm');
-const entriesSection = document.getElementById('entries');
-const entryTextbox = document.getElementsByClassName('entry-textbox');
-
-function addEntryToDom(event) {
-    event.preventDefault();
-    const entryDiv = document.createElement('div');
-    entryDiv.className = 'single-entry';
-    entryDiv.innerText = entryTextbox[0].value;
-    entriesSection.appendChild(entryDiv);
-  
-}
-
-entryForm.addEventListener('submit', addEntryToDom)
+var assignments = [log, dj, planet editor, loops];
+console.log(assignments)
